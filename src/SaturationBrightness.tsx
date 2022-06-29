6a465efcd4b69a8a5a198bcfdde304427dc30929
CIOSAI <jyinteractive.tw@gmail.com>
2022-06-29 16:59:10 +0800

    bruh

--- src/SaturationBrightness.tsx
+++ src/SaturationBrightness.tsx
@@ -1,6 +1,8 @@
 import React, { MouseEvent } from "react";
 import { useRef, useEffect, useState } from "react";
 import { hslToRgb, getColorString } from "./ColorConversion";
+import $ from "jquery";
+import "jqueryui";
 
 interface SatBriProps{
   hue: number
@@ -10,6 +12,8 @@ interface SatBriProps{
 export function SatBri(prop:SatBriProps){
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const canvasSize = 90
+  const [rectX, setRectX] = useState(0)
+  const [rectY, setRectY] = useState(0)
   const [saturation, setSaturation] = useState(0)
   const [brightness, setBrightness] = useState(0)
   const [dialing, setDialing] = useState(false)
@@ -43,6 +47,9 @@ export function SatBri(prop:SatBriProps){
     if(canvasRef.current){
       const context = canvasRef.current.getContext('2d')
       if (context) draw(context, canvasSize)
+      let rect = canvasRef.current.getBoundingClientRect()
+      setRectX(rect.x)
+      setRectY(rect.y)
     }
   }, [prop.hue, saturation, brightness])
 
@@ -52,22 +59,113 @@ export function SatBri(prop:SatBriProps){
     }
   }
 
+  let clamp = (val:number, min:number, max:number) => Math.max(Math.min(val, max), min)
   let dial = (evt:MouseEvent) => {
     if(!dialing) return
-    let rect = evt.currentTarget.getBoundingClientRect()
-    let [x, y] = [(evt.clientX-rect.x)/canvasSize, (evt.clientY-rect.y)/canvasSize]
+    let [x, y] = [clamp((evt.clientX-rectX)/canvasSize, 0, 1), clamp((evt.clientY-rectY)/canvasSize, 0, 1)]
     setSaturation( x )
     setBrightness( y )
+    console.log("hewwo")
     prop.onValueChanged(saturation, (1-x/2)*(1-brightness))
   }
 
+  $("#cursor").draggable({containment: "#SatBri", drag: ()=>{console.log("bruh")}})
   return (
+    <div id="sb">
     <canvas ref={canvasRef} id="SatBri" 
     width={canvasSize} height={canvasSize} 
-    onMouseDown={evt=>{setDialingState(true, evt)}}
-    onMouseUp={evt=>{setDialingState(false, evt)}}
-    onMouseLeave={evt=>{setDialingState(false, evt)}}
-    onMouseMove={evt=>{dial(evt)}}
     ></canvas>
+    <div id="cursor" draggable={true} onDrag={evt=>{dial(evt)}}></div>
+    </div>
   );
+
+  // use document.getElementById('id').innerHTML = 'text' to change text in a paragraph, for example.
+
+// var slider = {
+  
+//   get_position: function() {
+//     var marker_pos = $('#marker').position();
+//     var left_pos = marker_pos.left + slider.marker_size / 2;
+//     var top_pos = marker_pos.top + slider.marker_size / 2;
+    
+//     slider.position = {
+//       left: left_pos,
+//       top: top_pos,
+//       x: Math.round(slider.round_factor.x * (left_pos * slider.xmax / slider.width)) / slider.round_factor.x,
+//       y: Math.round((slider.round_factor.y * (slider.height - top_pos) * slider.ymax / slider.height)) / slider.round_factor.y,
+//     };
+
+//   },
+  
+//   display_position: function() {
+//     document.getElementById("coord").innerHTML = 'x: ' + slider.position.x.toString() + '<br> y: ' + slider.position.y.toString();
+//   },
+  
+//   draw: function(x_size, y_size, xmax, ymax, marker_size, round_to) {
+    
+//     if ((x_size === undefined) && (y_size === undefined) && (xmax === undefined) && (ymax === undefined) && (marker_size === undefined) && (round_to === undefined)) {
+//       x_size = 150;
+//       y_size = 150;
+//       xmax = 1;
+//       ymax = 1;
+//       marker_size = 20;
+//       round_to = 2;
+//     };
+    
+//     slider.marker_size = marker_size;
+//     slider.height = y_size;
+//     slider.width = x_size;
+//     slider.xmax = xmax;
+//     slider.ymax = ymax;
+//     round_to = Math.pow(10, round_to);
+//     slider.round_factor = {
+//       x: round_to,
+//       y: round_to,
+//     };
+    
+//     $("#markerbounds").css({
+//       "width": (x_size + marker_size).toString() + 'px',
+//       "height": (y_size + marker_size).toString() + 'px',
+//     });
+//     $("#box").css({
+//       "width": x_size.toString() + 'px',
+//       "height": y_size.toString() + 'px',
+//       "top": marker_size / 2,
+//       "left": marker_size / 2,
+//     });
+//     $("#marker").css({
+//       "width": marker_size.toString() + 'px',
+//       "height": marker_size.toString() + 'px',
+//     });
+
+//     $("#coord").css({
+//       "top": x_size + marker_size / 2
+//     });
+    
+//     $("#widget").css({
+//       "width": (x_size + marker_size).toString() + 'px',
+//     });
+    
+//     slider.get_position();
+//     slider.display_position();
+    
+//   },
+  
+// };
+
+// $("#marker").draggable({ 
+//   containment: "#markerbounds",
+//   drag: function() {
+//     slider.get_position();
+//     slider.display_position();
+//   },
+// });
+
+// //syntax for rendering is:
+// //  slider.render(width, height, width-range, height-range, marker size, output decimal places)
+
+// slider.draw(150,150,1,1,20,2);
+
+// // check to make sure the defaults work:
+// //slider.draw();
 }
